bcf98a8f2b174572c758d8fba63cf69f3f1e0016
Leo Shr <gogoloso123@gmail.com>
2022-03-17 15:05:30 +0800

    Luhn_validator & Caeser Part _Leo S

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -7,40 +7,34 @@ module SubstitutionCipher
     #   document: String
     #   key: Fixnum (integer)
     # Returns: String
-    
-    # Notice : I learned this method from Reddit.  
-    # https://www.reddit.com/r/ruby/comments/4ajwv8/is_there_a_better_way_to_caesar_cipher_then_this/
-    # ?a is actually the same as "a" , == ('a'..'z').to_a
-    # * expands the range into an argument list
+
+
+    # ---------- Method 2
     # CHARS = [*?A..?Z, *?a..?z]
 
-    def self.encrypt document, key
+    def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
-      # ---------- Method 1
-      # document.bytes.map! { |x| (x + key).chr }.join('')
-      # ---------- Method 2
+      # ---------- Method 2 Only swift the A..Z, a..z
+      # CHARS = [*?A..?Z, *?a..?z]
       # document.tr CHARS.join, CHARS.map{ |ary| ary.rotate key }.join
       # ---------- Method 3
-      shifted = document.chars.map { |c| c.ord }.map { |c| c + key }
-      shifted.map { |c| c.chr }.join
+      document.chars.map(&:ord).map { |c| c + key }.pack 'c*'
+      # Use .pack'c*' replace '.map { |c| c.chr }.join'
     end
 
-  #   # Decrypts String document using integer key
-  #   # Arguments:
-  #   #   document: String
-  #   #   key: Fixnum (integer)
-  #   # Returns: String
+    # Decrypts String document using integer key
+    # Arguments:
+    #   document: String
+    #   key: Fixnum (integer)
+    # Returns: String
 
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
-      # document.bytes.map! { |x| (x - key).chr }.join
-      # ---------- Method 1
-      # document.bytes.map! { |x| (x - key).chr }.join 
       #---------- Method 2  # Rotate in ASCII
       # document.tr CHARS.join, CHARS.map{ |cr| cr.rotate -key }.join
       # ---------- Method 3  # Only rotate in A..Z, a..z
-      shifted = document.chars.map { |c| c.ord }.map { |c| c - key }
-      shifted.map { |c| c.chr }.join
+      shifted = document.chars.map(&:ord).map { |c| c - key }
+      shifted.map(&:chr).join
     end
   end
 
