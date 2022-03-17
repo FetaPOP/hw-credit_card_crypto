6fd970f4b740852e1829f083bca718f3412b4dac
Huang Chuan <river1440>
2022-03-17 18:48:44 +0800

    create permutation cipher

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -8,7 +8,6 @@ module SubstitutionCipher
     #   key: Fixnum (integer)
     # Returns: String
 
-
     # ---------- Method 2
     # CHARS = [*?A..?Z, *?a..?z]
 
@@ -18,7 +17,7 @@ module SubstitutionCipher
       # CHARS = [*?A..?Z, *?a..?z]
       # document.tr CHARS.join, CHARS.map{ |ary| ary.rotate key }.join
       # ---------- Method 3
-      document.chars.map(&:ord).map { |c| c + key }.pack 'c*'
+      document.to_s.chars.map(&:ord).map { |c| c + key }.pack 'c*'
       # Use .pack'c*' replace '.map { |c| c.chr }.join'
     end
 
@@ -33,7 +32,7 @@ module SubstitutionCipher
       #---------- Method 2  # Rotate in ASCII
       # document.tr CHARS.join, CHARS.map{ |cr| cr.rotate -key }.join
       # ---------- Method 3  # Only rotate in A..Z, a..z
-      shifted = document.chars.map(&:ord).map { |c| c - key }
+      shifted = document.to_s.chars.map(&:ord).map { |c| c - key }
       shifted.map(&:chr).join
     end
   end
@@ -46,6 +45,9 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      document = document.to_s.chars
+      key_table = (0..127).to_a.shuffle!(random: Random.new(key))
+      document.map! { |x| key_table.at(x.ord).chr }.join
     end
 
     # Decrypts String document using integer key
@@ -55,6 +57,9 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      document = document.to_s.chars
+      key_table = (0..127).to_a.shuffle!(random: Random.new(key))
+      document.map! { |x| key_table.index(x.ord).chr }.join
     end
   end
 end
